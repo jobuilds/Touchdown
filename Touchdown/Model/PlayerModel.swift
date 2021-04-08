4e3371c0dd1e85501f4921932fd7385d31e9cb80
Jeren Ott <jobuilds@outlook.com>
2021-04-08 18:26:17 -0400

    created player model and Json decoder

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Jeren Ott on 4/8/21.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
