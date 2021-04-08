eafc54616c3cf79e96dbc67c93d5ac896f680d3b
Jeren Ott <jobuilds@outlook.com>
2021-04-08 18:59:50 -0400

    Created the CategoryGridView

---
+++ Touchdown/View/Home/CategoryGridView.swift
@@ -0,0 +1,38 @@
+//
+//  CategoryGridView.swift
+//  Touchdown
+//
+//  Created by Jeren Ott on 4/8/21.
+//
+
+import SwiftUI
+
+struct CategoryGridView: View {
+    //Properties
+    
+    //Body
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHGrid(rows: gridLayout, alignment: .center, spacing: columnSpacing, pinnedViews: [], content: {
+                ForEach(categories) { category in
+                    CategoryItemView(category: category)
+                }//Loop
+            })//LazyHGrid
+            .frame(height: 140)
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+        })//ScrollView
+    }
+}
+
+//Preview
+
+struct CategoryGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryGridView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(colorBackground)
+    }
+}
