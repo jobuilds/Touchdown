2238ba24fbd1783ddaf549542cb9612c86564c9c
Jeren Ott <jobuilds@outlook.com>
2021-04-08 15:09:17 -0400

    Initial commit

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Jeren Ott on 4/7/21.
+//
+
+import SwiftUI
+
+//Data
+//Color
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+//Layout
+//UX
+//API
+//Image
+//Font
+//String
+//Misc.
---
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,28 @@
+//
+//  NavigationBarView.swift
+//  Touchdown
+//
+//  Created by Jeren Ott on 4/8/21.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    //Properties
+    
+    //Body
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//Preview
+
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
